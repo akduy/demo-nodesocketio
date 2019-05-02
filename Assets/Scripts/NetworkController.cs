a73bb8a63ebdfad3661983e39465a629b7bea8da
Duy Tr <akduy2295@gmail.com>
2019-05-02 23:42:52 +0700

    Make connect, disconnect

---
+++ Assets/Scripts/NetworkController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using SocketIO;
+using UnityEngine;
+
+public class NetworkController : SocketIOComponent
+{
+    public override void Start()
+    {
+        base.Start();
+        SetEvents();
+    }
+
+    private void SetEvents()
+    {
+        On("open", (result) => 
+        {
+            this.Log("connected to server!!!");
+        });
+    }
+
+    public override void Update()
+    {
+        base.Update();
+    }
+}
